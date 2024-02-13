08e40b6875f19663a3729eca431479f62081df95
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-02-13 22:33:50 +0530

    commit1

--- index.js
+++ index.js
@@ -4,3 +4,39 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to MongoDB.."))
   .catch((err) => console.error("Could not connect to MongoDB", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "Angular Course",
+    author: "Mosh",
+    tags: ["angular", "frontend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+
+  console.log(result);
+}
+
+// createCourse();
+
+//Retrieving Data using query filtering
+async function getCourses() {
+  const course = await Course.find({ author: "Mosh", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(course);
+}
+
+getCourses();
